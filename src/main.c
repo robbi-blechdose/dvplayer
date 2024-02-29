20d4a7f6c5edef968c76ae46097a449e9d3e148f
Robbi Blechdose <robbi.blechdose@posteo.de>
2024-02-29 18:44:33 +0100

    Separated packet counting (with ff/rw) out into a function

--- src/main.c
+++ src/main.c
@@ -28,6 +28,8 @@
 
 #include <stdbool.h>
 
+#define NUM_FRAMES_FFRW 50
+
 #define DIF_BLOCK_SIZE 80
 #define DIF_BLOCKS_PER_SEQUENCE 150
 #define SEQUENCES_PER_FRAME_PAL  12
@@ -36,14 +38,17 @@
 #define PACKET_SIZE DIF_BLOCK_SIZE * 6
 
 const char* fileName = NULL;
+bool uiEnabled = true;
 
 static bool interrupted = false;
 
-static bool isPaused = false;
-static bool seekRequested = 0;
-static int seekDiff = 0;
+bool seekRequested = false;
+int seekDiff = 0;
+bool pauseChangeRequested = false;
+bool pauseRequested = false;
+bool isPaused = false;
 
-static int currentDIFBlocks = 0;
+static int currentPackets = 0;
 static int currentFrame = 0;
 
 //TODO: use a buffer to store a single full frame in to make pausing easier (meaning we don't have to keep reading the same frame from disk)
@@ -57,8 +62,46 @@ void seekFrame(FILE* file, int diff)
     fseek(file, diff * DIF_BLOCKS_PER_SEQUENCE * 2 * PACKET_SIZE, SEEK_CUR);
 }
 
-//TODO: this is not reading a full frame, but likely 6 DIF blocks
-static int readFrame(unsigned char *data, int n, unsigned int dropped, void *callback_data)
+void handlePacketCounters(FILE* file, unsigned char* data)
+{
+    //TODO: zeroing out the audio blocks seems to break things later down the line...
+    if(isPaused)
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            if((data[i * DIF_BLOCK_SIZE] >> 5) == 3 && data[i * DIF_BLOCK_SIZE + 2] < 9)
+            {
+                memset(data + i * DIF_BLOCK_SIZE + 3, 0, DIF_BLOCK_SIZE - 3);
+            }
+        }
+    }
+
+    currentPackets++;
+    if(currentPackets == DIF_BLOCKS_PER_SEQUENCE * 2)
+    {
+        currentPackets = 0;
+        currentFrame++;
+
+        if(isPaused)
+        {
+            //This will read the same bit over and over, thus "pausing" the video
+            seekFrame(file, -1);
+        }
+
+        if(pauseChangeRequested)
+        {
+            isPaused = pauseRequested;
+            pauseChangeRequested = false;
+        }
+        else if(seekRequested)
+        {
+            seekFrame(file, seekDiff);
+            seekRequested = false;
+        }
+    }
+}
+
+static int readPacket(unsigned char* data, int n, unsigned int dropped, void* callback_data)
 {
     FILE* file = (FILE*) callback_data;
 
@@ -69,35 +112,7 @@ static int readFrame(unsigned char *data, int n, unsigned int dropped, void *cal
 
     if(fread(data, PACKET_SIZE, 1, file) == 1)
     {
-        //TODO: zeroing out the audio blocks seems to break things later down the line...
-        if(isPaused)
-        {
-            for(int i = 0; i < 6; i++)
-            {
-                if((data[i * DIF_BLOCK_SIZE] >> 5) == 3 && data[i * DIF_BLOCK_SIZE + 2] < 9)
-                {
-                    memset(data + i * DIF_BLOCK_SIZE + 3, 0, DIF_BLOCK_SIZE - 3);
-                }
-            }
-        }
-
-        currentDIFBlocks++;
-        if(currentDIFBlocks == DIF_BLOCKS_PER_SEQUENCE * 2)
-        {
-            currentDIFBlocks = 0;
-            currentFrame++;
-
-            if(isPaused)
-            {
-                //This will read the same bit over and over, thus "pausing" the video
-                seekFrame(file, -1);
-            }
-            else if(seekRequested)
-            {
-                seekFrame(file, seekDiff);
-                seekRequested = false;
-            }
-        }
+        handlePacketCounters(file, data);
         return 0;
     }
     else
@@ -116,40 +131,43 @@ void handleInput(FILE* file)
     char c = getch();
     if(c == 'p')
     {
-        isPaused = !isPaused;
+        pauseChangeRequested = true;
+        pauseRequested = !isPaused;
     }
     else if(c == 'f')
     {
         seekRequested = true;
-        seekDiff = 50;
+        seekDiff = NUM_FRAMES_FFRW;
     }
     else if(c == 'r')
     {
         seekRequested = true;
-        seekDiff = -50;
+        seekDiff = -NUM_FRAMES_FFRW;
     }
 }
 
-void drawNcursesUI()
+void drawNcursesUI(bool isPAL)
 {
     char buffer[80];
     box(stdscr, 0, 0);
     mvaddstr(0, 8, "dvplayer");
     sprintf(buffer, "File: %s", fileName == NULL ? "stdin" : fileName);
     mvaddstr(1, 1, buffer);
-    sprintf(buffer, "Current frame: %5d Paused: %3s", currentFrame, isPaused ? "Yes" : "No");
+    sprintf(buffer, "Format: %s", isPAL ? "PAL" : "NTSC");
     mvaddstr(2, 1, buffer);
+    sprintf(buffer, "Current frame: %5d Paused: %3s", currentFrame, isPaused ? "Yes" : "No");
+    mvaddstr(3, 1, buffer);
     mvaddstr(8, 1, "P - Play/Pause   F - Forward 1s   R - Rewind 1s   Ctrl+C - Quit");
 }
 
-static void dv_transmit(raw1394handle_t handle, FILE* file, int channel)
+static void transmitDV(raw1394handle_t handle, FILE* file, int channel)
 {	
     unsigned char data[PACKET_SIZE];
     fread(data, PACKET_SIZE, 1, file);
 
     int isPAL = (data[3] & 0x80) != 0;
 
-    iec61883_dv_t dv = iec61883_dv_xmit_init(handle, isPAL, readFrame, (void*) file);
+    iec61883_dv_t dv = iec61883_dv_xmit_init(handle, isPAL, readPacket, (void*) file);
     if(dv == NULL)
     {
         return;
@@ -169,7 +187,10 @@ static void dv_transmit(raw1394handle_t handle, FILE* file, int channel)
     
     signal(SIGINT, sighandler);
     signal(SIGPIPE, sighandler);
-    fprintf(stderr, "Starting to transmit %s.\n", isPAL ? "PAL" : "NTSC");
+    if(!uiEnabled)
+    {
+        printf("Starting to transmit %s.\n", isPAL ? "PAL" : "NTSC");
+    }
 
     int result = 0;
     do
@@ -178,20 +199,28 @@ static void dv_transmit(raw1394handle_t handle, FILE* file, int channel)
         if((r = poll(&pfd, 1, 100)) > 0 && (pfd.revents & POLLIN))
         {
             result = raw1394_loop_iterate(handle);
-            handleInput(file);
-            drawNcursesUI();
+            if(uiEnabled)
+            {
+                handleInput(file);
+                drawNcursesUI(isPAL);
+            }
         }
         
     }
     while(!interrupted && result == 0);
     
     iec61883_dv_close(dv);
-    fprintf(stderr, "Done.\n");
+    if(!uiEnabled)
+    {
+        printf("Done.\n");
+    }
 }
 
-const char* helpText = "usage: dvplayer [-t node-id] [- | file]\n"
-                       "       Use - to transmit raw DV from stdin, or\n"
-                       "       supply a filename to to transmit from a raw DV file.\n";
+const char* helpText = "Usage: dvplayer [-n | --noui] [-t node-id] [- | file]\n"
+                       "Options:\n"
+                       "  -n | --noui  Disable ncurses UI. This also disables the input handling, only normal streaming is supported.\n"
+                       "  -t node-id   Supply a node ID for the FireWire device to stream to.\n"
+                       "  - | file     Read DV from stdin or from a file.\n";
 
 int main(int argc, char* argv[])
 {
@@ -202,10 +231,14 @@ int main(int argc, char* argv[])
 
     for(int i = 1; i < argc; i++)
     {
-        if(strncmp(argv[i], "-h", 2) == 0 ||  strncmp(argv[i], "--h", 3) == 0)
+        if(strncmp(argv[i], "-h", 2) == 0 ||  strncmp(argv[i], "--help", 6) == 0)
         {
-            fprintf(stderr, helpText);
+            printf(helpText);
             return 1;
+        }
+        else if(strncmp(argv[i], "-n", 2) == 0 ||strncmp(argv[i], "--noui", 6) == 0)
+        {
+            uiEnabled = false;
         }
         else if(strncmp(argv[i], "-t", 2) == 0)
         {
@@ -225,7 +258,7 @@ int main(int argc, char* argv[])
     
     if(!handle)
     {
-        fprintf(stderr, "Failed to get libraw1394 handle\n");
+        printf("Failed to get libraw1394 handle.\n");
         if(inputFile != NULL)
         {
             fclose(inputFile);
@@ -240,35 +273,40 @@ int main(int argc, char* argv[])
         inputFile = stdin;
     }
 
-    //Set up ncurses
-    initscr();
-    noecho();
-    curs_set(0);
-    keypad(stdscr, true);
-    timeout(0);
+    if(uiEnabled)
+    {
+        //Set up ncurses
+        initscr();
+        noecho();
+        curs_set(0);
+        keypad(stdscr, true);
+        timeout(0);
+    }
     
     if(nodeSpecified)
     {
         channel = iec61883_cmp_connect(handle, raw1394_get_local_id(handle), &oplug, node, &iplug, &bandwidth);
         if(channel > -1)
         {
-            fprintf(stderr, "Connect succeeded, transmitting on channel %d.\n", channel);
-            dv_transmit(handle, inputFile, channel);
+            printf("Connect succeeded, transmitting on channel %d.\n", channel);
+            transmitDV(handle, inputFile, channel);
             iec61883_cmp_disconnect(handle, raw1394_get_local_id(handle), oplug, node, iplug, channel, bandwidth);
         }
         else
         {
-            fprintf(stderr, "Connect failed, reverting to broadcast channel 63.\n");
-            dv_transmit(handle, inputFile, 63);
+            printf("Connect failed.\n");
         }
     }
     else
     {
-        dv_transmit(handle, inputFile, 63);
+        transmitDV(handle, inputFile, 63);
     }
 
-    //Quit ncurses
-    endwin();
+    if(uiEnabled)
+    {
+        //Quit ncurses
+        endwin();
+    }
 
     if(inputFile != stdin)
     {
